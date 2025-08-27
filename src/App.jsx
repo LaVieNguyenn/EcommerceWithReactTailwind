494b87b8cb5ecade60e3052a26efa8f3b1bec4d9
LaVieNguyenn <hungnpse172907@fpt.edu.vn>
2025-08-27 17:06:28 +0700

    feat: Add Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Narbar from './components/Navbar/Narbar'
+import Hero from './components/Hero/Hero'
 
 const App = () => {
   return (
     <div>
       <Narbar/>
+      <Hero/>
     </div>
   )
 }
