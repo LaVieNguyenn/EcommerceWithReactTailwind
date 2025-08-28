f933c0ed85a33d25a39b489ca8bfdee6659afb48
LaVieNguyenn <hungnpse172907@fpt.edu.vn>
2025-08-28 13:26:05 +0700

    feat: Add Product Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Narbar from './components/Navbar/Narbar'
 import Hero from './components/Hero/Hero'
+import Products from './components/Products/Products'
+import Aos from 'aos'
+import "aos/dist/aos.css"
 
 const App = () => {
+  React.useEffect(() => {
+    Aos.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    })
+  }, []);
+
   return (
     <div>
       <Narbar/>
       <Hero/>
+      <Products/>
     </div>
   )
 }
